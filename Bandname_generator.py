05dc3de9b43d62414d767cdbf5504092a97279b2
Victorynwobiakc <victorynwobiakc@gmail.com>
2026-04-04 18:06:18 +0100

    band name generator

--- Bandname_generator.py
+++ Bandname_generator.py
@@ -1,1 +1,6 @@
-print("hello world")
+print("This is a band name generator.")
+
+city = input("what city did you grow up in? \n")
+animal = input("what's your pets name? \n")
+
+print(f"Your band name could be {city} {animal}")
