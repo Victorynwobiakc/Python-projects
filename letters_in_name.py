50e290d26c468f60437bef105d0a0401f66f51b8
Victorynwobiakc <victorynwobiakc@gmail.com>
2026-04-04 18:12:27 +0100

    name character counter

---
+++ letters_in_name.py
@@ -0,0 +1,8 @@
+# how many letters are in your first name
+
+print("Name character counter \n")
+first_name = input("what's your first name? ")
+
+char_count = len(first_name)
+
+print(f"There are {char_count} in your name")
